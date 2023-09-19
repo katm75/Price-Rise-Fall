1914c1f987f42a9b2e8e51764c0639dac40eaefd
Kathryn <141536562+katm75@users.noreply.github.com>
2023-09-18 23:02:31 -0400

    Set up the file for the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # price-rise-fall
 Programming Exercise 3
+https://github.com/katm75/price-rise-fall
---
+++ price-rise-fall.py
@@ -0,0 +1,25 @@
+# This is exercise 3 from https://github.com/py-study-group/beginner-friendly-programming-exercises/blob/master/exercises.md
+    # Modified to change "Bitcoin" to "Stock"
+
+# Kathryn Marks
+# kathryn.pythonprograms@gmail.com
+# Created September 18, 2023
+# Last modified September 18, 2023
+
+# #You've bought some stock and now the price is on the rise
+
+# Create a program that:
+
+# Reads the value of the stock at the time of purchase
+# Reads the percentage of increase (or decrease)
+# Prints the total value of your stock
+# Prints the increase or decrease value
+
+# Example: bitcoin_value = 10000, bitcoin_increase = 10
+# Output: total_bitcoin_value = 11000, bitcoin_increase_value = 1000
+
+# Define the variables
+    # price = the current price of the stock
+    # price_change = the increase or decrease of the stock
+    # total_value = the new value of the stock
+
